18a82ba79a291085abd3c8f8af65644799211769
Oderjunkie <58880677+Oderjunkie@users.noreply.github.com>
2021-05-21 22:39:02 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 USERNAME = 'hackfate'
 PASSWORD = 'REDACTED' # no pass 4 u
-ROOM     = 
+ROOM     = None # none = make new room, string = enter existing room
 AUTH     = [USERNAME, 'Otesunkie', 'Poggles', 'moontree', 'camelCasedSnivy'] # bot acc first, main acc for host second, then alts for host, then other people given auth.
 UNLISTED = False
 ROOMNAME = 'Cookie Club! (V.3)'
