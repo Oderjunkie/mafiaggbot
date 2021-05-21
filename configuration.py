a936ca7865883da9b5ab5a1c928f33cd508312fc
Oderjunkie <58880677+Oderjunkie@users.noreply.github.com>
2021-05-21 22:37:33 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,7 @@
+USERNAME = 'hackfate'
+PASSWORD = 'REDACTED' # no pass 4 u
+ROOM     = 
+AUTH     = [USERNAME, 'Otesunkie', 'Poggles', 'moontree', 'camelCasedSnivy'] # bot acc first, main acc for host second, then alts for host, then other people given auth.
+UNLISTED = False
+ROOMNAME = 'Cookie Club! (V.3)'
+AUTHREQ = True
