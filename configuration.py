c0d2d98f701c608ae8c43c1a2c41aa2fd151b8a3
Oderjunkie <58880677+Oderjunkie@users.noreply.github.com>
2021-05-24 00:51:03 +0300

    Delete configuration.py

--- configuration.py
+++
@@ -1,7 +0,0 @@
-USERNAME = 'hackfate'
-PASSWORD = 'REDACTED' # no pass 4 u
-ROOM     = None # none = make new room, string = enter existing room
-AUTH     = [USERNAME, 'Otesunkie', 'Poggles', 'moontree', 'camelCasedSnivy'] # bot acc first, main acc for host second, then alts for host, then other people given auth.
-UNLISTED = False
-ROOMNAME = 'Cookie Club! (V.3)'
-AUTHREQ = True
